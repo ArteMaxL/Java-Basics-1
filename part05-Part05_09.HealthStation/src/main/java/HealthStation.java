a1a8fc9a940895b9876d6470f6f9c126c042bb80
ArteMaxL <78069468+ArteMaxL@users.noreply.github.com>
2022-02-13 18:01:56 -0300

    Completo hasta parte 5.10

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,19 @@
 
 public class HealthStation {
-
-
+    private int countWeighings = 0;
+    
     public int weigh(Person person) {
+        countWeighings++;
         // return the weight of the person passed as the parameter
-        return -1;
+        return person.getWeight();
     }
 
+    public void feed(Person person) {
+        int addWeigh = this.weigh(person);
+        addWeigh+=1;
+        person.setWeight(addWeigh);
+    }
+    public int weighings(){
+        return countWeighings;
+    }
 }
